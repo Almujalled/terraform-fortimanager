0f081718a8c8c8433feac97b29cbaccefd721bf2
Almujalled <ghaith.almujalled@hotmail.com>
2023-10-08 19:39:10 +0000

    added CLI Template Groups

--- main.tf
+++ main.tf
@@ -46,6 +46,76 @@ resource "fortimanager_object_cli_template" "Edge-Underlay" {
   depends_on  = [fortimanager_object_cli_template.project]
 }
 
+resource "fortimanager_object_cli_template" "Hub-Underlay" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "01-Hub-Underlay.j2"
+  script      = file("cli-templates/01-Hub-Underlay.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_template" "Edge-Overlay" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "02-Edge-Overlay.j2"
+  script      = file("cli-templates/02-Edge-Overlay.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_template" "Hub-Overlay" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "02-Hub-Overlay.j2"
+  script      = file("cli-templates/02-Hub-Overlay.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_template" "Edge-Routing" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "03-Edge-Routing.j2"
+  script      = file("cli-templates/03-Edge-Routing.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_template" "Hub-Routing" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "03-Hub-Routing.j2"
+  script      = file("cli-templates/03-Hub-Routing.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_template" "Hub-MultiRegion" {
+  description = "This is a Terraform example to build a CLI Template"
+  name        = "04-Hub-MultiRegion.j2"
+  script      = file("cli-templates/04-Hub-MultiRegion.j2")
+  type        = "jinja"
+  depends_on  = [fortimanager_object_cli_template.project]
+}
+
+resource "fortimanager_object_cli_templategroup" "Edge-Template" {
+  description = "This is a Terraform example to build a CLI template group"
+  member      = ["01-Edge-Underlay.j2","02-Edge-Overlay.j2","03-Edge-Routing.j2"]
+  name        = "Edge-Template"
+  depends_on = [
+    fortimanager_object_cli_template.Edge-Overlay,
+    fortimanager_object_cli_template.Edge-Routing,
+    fortimanager_object_cli_template.Edge-Underlay
+  ]
+}
+
+resource "fortimanager_object_cli_templategroup" "Hub-Template" {
+  description = "This is a Terraform example to build a CLI template group"
+  member      = ["01-Hub-Underlay.j2","02-Hub-Overlay.j2","03-Hub-Routing.j2"]
+  name        = "Hub-Template"
+  depends_on = [
+    fortimanager_object_cli_template.Hub-Overlay,
+    fortimanager_object_cli_template.Hub-Routing,
+    fortimanager_object_cli_template.Hub-Underlay
+  ]
+}
+
 resource "fortimanager_exec_workspace_action" "unlockres" { # save change and unlock root ADOM
   scopetype      = "inherit"
   action         = "lockend"
@@ -53,5 +123,5 @@ resource "fortimanager_exec_workspace_action" "unlockres" { # save change and un
   param          = ""
   force_recreate = uuid()
   comment        = ""
-  depends_on     = [fortimanager_object_cli_template.Edge-Underlay]
+  depends_on     = [fortimanager_object_cli_templategroup.Hub-Template]
 }
