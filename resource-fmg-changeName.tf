354be75a0270df8106ba92407fb5a0dfc34c36ab
Almujalled <ghaith.almujalled@hotmail.com>
2023-10-12 12:59:31 +0000

    enhancing changeName to include all feilds!

--- resource-fmg-changeName.tf
+++ resource-fmg-changeName.tf
@@ -14,16 +14,18 @@ resource "fortimanager_json_generic_api" "changeName" {
   "params": [
     {
       "data": {
+        "adm_pass": "",
+        "adm_usr": "admin",
         "desc": "This is updated from Terrafrom using an API!",
-        "latitude": 10,
-        "longitude": 10,
+        "latitude": 60.37309404359674,
+        "longitude": 5.33920881412722,
         "meta fields": {
           "Address": "Damascus",
           "Company/Organization": "Eviny Digital AS",
           "Contact Email": "ghaith.almujalled@eviny.no",
           "Contact Phone Number": "+4745402619"
         },
-        "name": "UpdateDeviceFromTerra"
+        "name": "UpdatedFromTerra"
       },
       "url": "/dvmdb/adom/GNS3/device/NewNameFromTerra"
     }
@@ -35,3 +37,33 @@ JSON
     fortimanager_dvm_cmd_update_device.refreshDevice
   ]
 }
+
+## Ref: Reverse engineer a GUI request via CLI debug:
+#{
+#  "client": "gui json:11231",
+#  "id": "5b8d7674-f3bc-4e85-94fc-e4fd1e3d825f",
+#  "keep_session_idle": 1,
+#  "method": "update",
+#  "params": [
+#    {
+#      "data": {
+#        "adm_pass": "******",
+#        "adm_usr": "admin",
+#        "desc": "",
+#        "ip": "10.1.10.245",
+#        "latitude": 0,
+#        "longitude": 0,
+#        "meta fields": {
+#          "Address": "",
+#          "Company\\/Organization": "",
+#          "Contact Email": "",
+#          "Contact Phone Number": ""
+#        },
+#        "name": "TEST-CHANGE-GUI"
+#      },
+#      "target start": 1,
+#      "url": "\\/dvmdb\\/adom\\/GNS3\\/device\\/FGVMEVKZQU851F62"
+#    }
+#  ],
+#  "session": 21474
+#} 
