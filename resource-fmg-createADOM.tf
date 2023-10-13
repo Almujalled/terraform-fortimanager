3eb35f74812d2cc3128c208b963bf376820ce1ad
Almujalled <ghaith.almujalled@hotmail.com>
2023-10-13 11:13:12 +0000

    test

--- resource-fmg-createADOM.tf
+++ resource-fmg-createADOM.tf
@@ -26,6 +26,34 @@ resource "fortimanager_dvmdb_adom" "createADOM" {
   ]
 }
 
+resource "fortimanager_dvmdb_adom" "createADOM" {
+  create_time = 0
+  flags = [
+    "no_vpn_console",
+  ]
+  log_db_retention_hours     = 1440
+  log_disk_quota             = 0
+  log_disk_quota_alert_thres = 90
+  log_disk_quota_split_ratio = 70
+  log_file_retention_hours   = 8760
+  mig_mr                     = 0
+  mig_os_ver                 = "0.0"
+  mode                       = "gms"
+  mr                         = 2
+  name                       = "GNS3"
+  os_ver                     = "7.0"
+  restricted_prds = [
+    "fos",
+  ]
+  state          = 1
+  tz             = 0
+  uuid           = ""
+  workspace_mode = 1
+  depends_on = [
+  fortimanager_exec_workspace_action.lockROOT
+  ]
+}
+
 #resource "fortimanager_json_generic_api" "createADOM" {
 #  json_content = <<JSON
 #{
