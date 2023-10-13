b8c97e17119b019668bfc312e51f729caa7248ea
Almujalled <ghaith.almujalled@hotmail.com>
2023-10-13 13:26:06 +0000

    add metadata var

---
+++ resource-fmg-variables.tf
@@ -0,0 +1,10 @@
+resource "fortimanager_object_fmg_variable" "createMetadata" {
+  description = ""
+  name        = inbandwidth
+  scopetype   = "adom"
+  adom        = var.workingADOM
+  depends_on  = [
+    fortimanager_dvmdb_adom.createADOM,
+    fortimanager_exec_workspace_action.lockWorkingADOM
+    ]
+}
