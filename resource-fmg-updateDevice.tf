4d3afd407ed5b88218ecea94a6ed0c354f5a08db
Almujalled <ghaith.almujalled@hotmail.com>
2023-10-19 15:21:36 +0000

    test add device

--- resource-fmg-updateDevice.tf
+++ resource-fmg-updateDevice.tf
@@ -1,41 +1,41 @@
-#resource "fortimanager_dvm_cmd_update_device" "refreshDevice" {
-#  fmgadom = var.workingADOM
-#  device  = var.deviceInfo.platform_str
-#  flags   = ["create_task", "nonblocking"]
-#  depends_on = [
-#    fortimanager_json_generic_api.authDevice
-#  ]
-#}
-#
-#resource "fortimanager_json_generic_api" "updateDeviceDB" {
-#  json_content = <<JSON
-#{
-#  "method": "update",
-#  "params": [
-#    {
-#      "data": {
-#        "adm_pass": "${var.deviceInfo.adm_pass}",
-#        "adm_usr": "${var.deviceInfo.adm_usr}",
-#        "desc": "This is updated from Terrafrom using an API!",
-#        "latitude": 60.37309404359674,
-#        "longitude": 5.33920881412722,
-#        "meta fields": {
-#          "Address": "Fjøsangerveien 65",
-#          "Company/Organization": "Eviny Digital AS",
-#          "Contact Email": "ghaith.almujalled@verja.no",
-#          "Contact Phone Number": "+4745402619"
-#        },
-#        "name": "${var.deviceInfo.name}"
-#      },
-#      "url": "/dvmdb/adom/${var.workingADOM}/device/${var.deviceInfo.platform_str}"
-#    }
-#  ]
-#}
-#JSON
-#  depends_on = [
-#    fortimanager_exec_workspace_action.lockADOM
-#  ]
-#}
+resource "fortimanager_dvm_cmd_update_device" "refreshDevice" {
+  fmgadom = var.workingADOM
+  device  = var.deviceInfo.platform_str
+  flags   = ["create_task", "nonblocking"]
+  depends_on = [
+    fortimanager_json_generic_api.authDevice
+  ]
+}
+
+resource "fortimanager_json_generic_api" "updateDeviceDB" {
+  json_content = <<JSON
+{
+  "method": "update",
+  "params": [
+    {
+      "data": {
+        "adm_pass": "${var.deviceInfo.adm_pass}",
+        "adm_usr": "${var.deviceInfo.adm_usr}",
+        "desc": "This is updated from Terrafrom using an API!",
+        "latitude": 60.37309404359674,
+        "longitude": 5.33920881412722,
+        "meta fields": {
+          "Address": "Fjøsangerveien 65",
+          "Company/Organization": "Eviny Digital AS",
+          "Contact Email": "ghaith.almujalled@verja.no",
+          "Contact Phone Number": "+4745402619"
+        },
+        "name": "${var.deviceInfo.name}"
+      },
+      "url": "/dvmdb/adom/${var.workingADOM}/device/${var.deviceInfo.platform_str}"
+    }
+  ]
+}
+JSON
+  depends_on = [
+    fortimanager_exec_workspace_action.lockADOM
+  ]
+}
 
 ## Ref: Reverse engineer a GUI request via CLI debug:
 #{
